116991af83eefb018a27b68fed29485e345c3c3c
asatasinon <asatasinon@gmail.com>
2020-10-10 11:29:00 +0800

    添加对应的注释，修改readme

--- src/main/java/com/sinon/hello/HelloApplication.java
+++ src/main/java/com/sinon/hello/HelloApplication.java
@@ -9,16 +9,18 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+//mybatis的mapper文件扫描
 @MapperScan("com.sinon.hello.mapper")
+//springboot程序入口
+@SpringBootApplication
 //语句的意义：排除 数据源的自动初始化配置 ；但经测试，不排除也没问题
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// 启用定时任务
 @EnableScheduling
-@SpringBootApplication
+// 启用缓存
 @EnableCaching
 public class HelloApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
-
 }
--- src/main/java/com/sinon/hello/mapper/AnnouncementMapper.java
+++ src/main/java/com/sinon/hello/mapper/AnnouncementMapper.java
@@ -4,6 +4,7 @@ import com.sinon.hello.annotation.MasterDataSource;
 import com.sinon.hello.annotation.SlaveDataSource;
 import com.sinon.hello.enums.BalanceTypeEnum;
 import com.sinon.hello.entity.AnnouncementDO;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +14,11 @@ import org.springframework.stereotype.Repository;
  * @Description AnnouncementMapper
  * @CreateDate 2020/9/23
  */
+@CacheConfig(cacheNames = "announcement")
 @Repository
 public interface AnnouncementMapper {
 
-    @Cacheable(cacheNames = "announcement", key = "#id")
+    @Cacheable(key = "#id")
     @MasterDataSource()
     AnnouncementDO selectTest(int id);
 
